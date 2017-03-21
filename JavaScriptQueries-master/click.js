ad4dfa079afe13f67ef09febcab4108f8b50906e
9Kevinzhao <9kzhao@stpatrick.org>
2017-03-21 13:59:31 -0500

    Add files via upload

---
+++ JavaScriptQueries-master/click.js
@@ -0,0 +1,12 @@
+//declare samecolor() here
+
+function samecolor(){
+const doc = document
+thing1 =doc.querySelectorAll("#samecolor div")
+thing1[0].style.backgroundColor = #555555;
+thing1[1].style.backgroundColor = #555555;
+thing1[2].style.backgroundColor = #555555;
+thing1[3].style.backgroundColor = #555555;
+
+}
+//declare diffcolor() here
