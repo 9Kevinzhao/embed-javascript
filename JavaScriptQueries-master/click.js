38003a6c5c113826026719d999ad844c87b281c4
9Kevinzhao <9kzhao@stpatrick.org>
2017-03-22 14:01:51 -0500

    Add files via upload

--- JavaScriptQueries-master/click.js
+++ JavaScriptQueries-master/click.js
@@ -3,10 +3,28 @@
 function samecolor(){
 const doc = document
 thing1 =doc.querySelectorAll("#samecolor div")
-thing1[0].style.backgroundColor = #555555;
-thing1[1].style.backgroundColor = #555555;
-thing1[2].style.backgroundColor = #555555;
-thing1[3].style.backgroundColor = #555555;
-
+thing1[0].style.backgroundColor = "#009900";
+thing1[1].style.backgroundColor = "#009900";
+thing1[2].style.backgroundColor = "#009900";
+thing1[3].style.backgroundColor = "#009900";
+thing1[0].style.opacity =0.7;
+thing1[1].style.opacity =0.6;
+thing1[2].style.opacity =0.5;
+thing1[3].style.opacity =0.4;
 }
 //declare diffcolor() here
+
+function diffcolor(){
+const doc = document
+thing2 =doc.querySelectorAll("#diffcolor div")
+thing2[0].style.backgroundColor =  "rgb(255, 255, 0)";
+thing2[1].style.backgroundColor =  "rgb(0, 0, 255)";
+thing2[2].style.backgroundColor =  "rgb(255, 255, 0)";
+thing2[3].style.backgroundColor =  "rgb(0, 0, 255)";
+thing2[4].style.backgroundColor =  "rgb(255, 255, 0)";
+thing2[5].style.backgroundColor =  "rgb(0, 0, 255)";
+thing2[6].style.backgroundColor =  "rgb(255, 255, 0)";
+thing2[7].style.backgroundColor =  "rgb(0, 0, 255)";
+
+
+}
--- JavaScriptQueries-master/resize.js
+++ JavaScriptQueries-master/resize.js
@@ -1,4 +1,13 @@
 //Declare tabletSize() here
+function tableSize(){
+  const doc = document
+  thing3 =doc.getElementsByTagName('html')
+  for(var i=0; i<thing3; i++){
+    var width= thing3[i].getPropertyValue('width')
+    var height= thing3[i].getPropertyValue('height')
+    
+  }
 
+}
 
 //Declare phoneSize() here
