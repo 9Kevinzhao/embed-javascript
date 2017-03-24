87925c07b3bd053054555d1af8fa7a327db454e6
9Kevinzhao <9kzhao@stpatrick.org>
2017-03-24 13:58:18 -0500

    Add files via upload

--- JavaScriptQueries-master/resize.js
+++ JavaScriptQueries-master/resize.js
@@ -1,16 +1,26 @@
 //Declare tabletSize() here
 function tabletSize(){
   const doc = document
-  thing3 =doc.getElementsByTagName('div')
-  thing3[0].style.width = "75%";
-  console.log(thing3[0].style.width)
-  /*for(var i=0; i<thing3; i++){
-    var width = thing3[i].style.width
-    var height = thing3[i].style.height
-    height = height*0.75
-    width = width*0.75
-  }*/
+  thing3 =doc.getElementsByTagName('section')
+//thing3[0].style.width="18.75%";
+  for(var i=0; i<thing3.length; i++){
+    thing3[i].style.width = "1080px"
+    thing3[i].style.height = "600px"
+
+  }
+
 }
 
 
 //Declare phoneSize() here
+function phoneSize(){
+  const doc = document
+  thing3 =doc.getElementsByTagName('section')
+//thing3[0].style.width="18.75%";
+  for(var i=0; i<thing3.length; i++){
+    thing3[i].style.width = "720spx"
+    thing3[i].style.height = "400px"
+
+  }
+
+}
